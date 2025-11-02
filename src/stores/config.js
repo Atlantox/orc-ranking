4212dadf8c7dc949041fc43f4e6cb74980cc2a7d
Atlantox <atlantox7@gmail.com>
2025-11-01 20:24:29 -0400

    <fix> changing connection to correct url

--- src/stores/config.js
+++ src/stores/config.js
@@ -1,7 +1,7 @@
 class ApiConfig{
     constructor(){
-        this.base_url ='http://localhost:5001'
-        //this.base_url ='https://wikiwikiexample.pythonanywhere.com'
+        //this.base_url ='http://localhost:5001'
+        this.base_url ='https://wikiwikiexample.pythonanywhere.com'
     }
 }
 
